7b8520d3e3a5a79fe8a55f5497e2fd8fca9ea12f
DejVV <33099962+dawidtanski@users.noreply.github.com>
2026-01-04 22:28:56 +0100

    Add files via upload
    ex00 finished

--- ex00/inc/BitcoinExchange.hpp
+++ ex00/inc/BitcoinExchange.hpp
@@ -3,11 +3,14 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <fstream>
+#include <cstdlib>
 
 class BitcoinExchange{
 
 private:
-	std::map<std::string, float> _database;
+	std::multimap<std::string, float> _database;
+	std::multimap<std::string, float> _databaseCSV;
 
 public:
     // Orthodox Canonical Form
@@ -16,8 +19,16 @@ public:
     BitcoinExchange& operator=(const BitcoinExchange& other);
     ~BitcoinExchange();
 
-	void loadData(std::fstream filestream);
-	void mapPush(std::string date, float price);
+	void loadData();
+	void mapPush(std::string date, float value);
 	void mapIterCout() const;
 	float getExchangeRate(std::string date);
+	float	getClosestRate(std::string date);
 };
+
+
+// Helpers
+int dateToInt(const std::string &s);
+bool isNumber(const std::string &s);
+bool dateValidation(std::string date);
+bool valueValidation(float value);
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -1,7 +1,9 @@
 #include "../inc/BitcoinExchange.hpp"
 
 // Orthodox Canonical Form
-BitcoinExchange::BitcoinExchange() {}
+BitcoinExchange::BitcoinExchange() {
+	loadData();
+}
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) {
 	_database = other._database;
@@ -16,8 +18,8 @@ BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other) {
 
 BitcoinExchange::~BitcoinExchange() {}
 
-void	BitcoinExchange::mapPush(std::string date, float price){
-	_database[date] = price;
+void	BitcoinExchange::mapPush(std::string date, float value){
+	_database.insert(std::make_pair(date, value));
 }
 
 void	BitcoinExchange::mapIterCout() const{
@@ -26,8 +28,116 @@ void	BitcoinExchange::mapIterCout() const{
 	}
 }
 
-// void BitcoinExchange::loadData(std::fstream filestream){
-// 	std::string date;
+void BitcoinExchange::loadData(){
+	std::ifstream file;
+	file.open("data.csv");
+	if (!file.is_open()){
+		std::cerr << "Cannot open the file!" << std::endl;
+		return ;
+	}
 
-// 	std::getline(filestream, date, '|');
-// }
+	std::string line;
+	if (!std::getline(file, line))
+		return ;
+	while (std::getline(file, line)){
+			if (line.empty())
+				continue;
+		size_t pos = line.find(",");
+		if (pos == std::string::npos){
+			// std::cerr << "Error: bad input => " << line << std::endl;
+			continue;
+		}
+		std::string date = line.substr(0, pos);
+		std::string valueStr = line.substr(pos + 1);
+		float value = atof(valueStr.c_str());
+		if (!dateValidation(date))
+			continue;
+		if (value < 0)
+			continue;
+		_databaseCSV.insert(std::make_pair(date, value));
+	}
+
+}
+
+float	BitcoinExchange::getClosestRate(std::string date){
+	if (_databaseCSV.empty())
+		return 0;
+	
+	std::multimap<std::string, float>::iterator it = _databaseCSV.lower_bound(date);
+
+	if (it != _databaseCSV.end() && it->first == date)
+		return it->second;
+	
+
+	if (it == _databaseCSV.end()) {
+		--it;
+		return it->second;
+	}
+	
+	if (it == _databaseCSV.begin())
+		return 0;
+	
+
+	--it;
+	return it->second;
+}
+
+
+
+
+
+/// Helpers
+
+int dateToInt(const std::string &s)
+{
+	int y = (s[0]-'0')*1000 + (s[1]-'0')*100
+		+ (s[2]-'0')*10   + (s[3]-'0');
+	int m = (s[5]-'0')*10   + (s[6]-'0');
+	int d = (s[8]-'0')*10   + (s[9]-'0');
+
+	return y * 10000 + m * 100 + d;
+}
+
+bool isNumber(const std::string &s)
+{
+    char *end;
+    strtol(s.c_str(), &end, 10);
+    return end != s.c_str() && *end == '\0';
+}
+
+bool	dateValidation(std::string date){
+	if (date.length() != 10)
+		return false;
+	if (date[4] != '-' || date[7] != '-')
+		return false;
+	std::string datecp = date;
+	datecp.erase(7,1);
+	datecp.erase(4,1);
+	if (!isNumber(datecp))
+		return false;
+	if ((1000 * (date[0] - '0') + 100 * (date[1] - '0')
+	+ 10 * (date[2] - '0') + (date[3] - '0')) > 2026 ||
+	(1000 * (date[0] - '0') + 100 * (date[1] - '0')
+	+ 10 * (date[2] - '0') + (date[3] - '0')) <= 0)
+		return false;
+	if ((10 * (date[5] - '0') + (date[6] - '0')) > 12 || 
+		(10 * (date[5] - '0') + (date[6] - '0')) <= 0)
+		return false;
+	if ((10 * (date[8] - '0') + (date[9] - '0')) > 31 || 
+		(10 * (date[8] - '0') + (date[9] - '0')) <= 0)
+		return false;
+	// std::cout << "Date-month-day valid format" << std::endl;
+	return true;
+}
+
+bool valueValidation(float value){
+	if (value < 0){
+		std::cerr << "Error: not a positive number" << std::endl;
+		return false;
+	}
+	if (value > 1000){
+		std::cerr << "Error: too large a number" << std::endl;
+		return false;
+	}
+	return true;
+}
